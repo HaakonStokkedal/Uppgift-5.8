9354b9cbd24bf53d219129c24d4a581548fcef28
Håkon <144228803+HaakonStokkedal@users.noreply.github.com>
2023-11-27 19:49:00 +0100

    Add project files.

---
+++ Uppgift 5.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Uppgift_5._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nummer = new int[5];
+            Random rnd = new Random();
+            for (int i = 0; i < nummer.Length; i++)
+            {
+                nummer[i] = rnd.Next(10, 99);
+            }
+            for(int i = 0;i < nummer.Length;i++)
+            {
+                PrintNumber(nummer[i]);
+                if(nummer.Length - 1 != i)
+                    Task.Delay(1500).Wait();
+
+            }
+        }
+
+        static void PrintNumber(int num)
+        {
+            var text = num.ToString();
+            for (int i = 0; i < text.Length; i++)
+            {
+                var t = i != text.Length - 1 ? text[i].ToString() : $"{text[i]}\n";
+                Console.Write(t);
+                Task.Delay(500).Wait();
+            }
+        }
+    }
+}
